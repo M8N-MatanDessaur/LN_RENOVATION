0a3fdb270d071be7bda74602ec4a2fe2e00b0644
Matan Dessaur <matandessaur@m8ndoes.com>
2023-06-17 19:21:54 -0400

    performance fix

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -7,46 +7,45 @@ import imgc from "../Images/c.jpg";
 import imgd from "../Images/d.jpg";
 import imge from "../Images/e.jpg";
 
-
 export default function Services() {
-    return (
-        <>
-            <Title>Nos services</Title>
-            <ServicesSection>
-                <ServiceCard image={imge}>
-                    <ServiceCardTitle>Géstion de projet</ServiceCardTitle>
-                </ServiceCard>
-                <ServiceCard image={imgd}>
-                    <ServiceCardTitle>Revetement de plancher</ServiceCardTitle>
-                </ServiceCard>
-                <ServiceCard image={imgc}>
-                    <ServiceCardTitle>Travaux de finition.</ServiceCardTitle>
-                </ServiceCard>
-                <ServiceCard image={imgb}>
-                    <ServiceCardTitle>Cuisine et salle de bains</ServiceCardTitle>
-                </ServiceCard>
-                <ServiceCard image={imga}>
-                    <ServiceCardTitle>Excavation et terrasement</ServiceCardTitle>
-                </ServiceCard>
-                <OtherServiceCard>
-                    <h2 style={{color:"#fff"}}>Et plus encore...</h2>
-                    <OtherServiceTitle>Drains Français</OtherServiceTitle>
-                    <OtherServiceTitle>Dalle de béton</OtherServiceTitle>
-                    <OtherServiceTitle>Pavé</OtherServiceTitle>
-                    <OtherServiceTitle>Démolition</OtherServiceTitle>
-                </OtherServiceCard>
-            </ServicesSection>
-        </>
-    )
+  return (
+    <>
+      <Title>Nos services</Title>
+      <ServicesSection>
+        <ServiceCard image={imge}>
+          <ServiceCardTitle>Gestion de projet</ServiceCardTitle>
+        </ServiceCard>
+        <ServiceCard image={imgd}>
+          <ServiceCardTitle>Revetement de plancher</ServiceCardTitle>
+        </ServiceCard>
+        <ServiceCard image={imgc}>
+          <ServiceCardTitle>Travaux de finition</ServiceCardTitle>
+        </ServiceCard>
+        <ServiceCard image={imgb}>
+          <ServiceCardTitle>Cuisine et salle de bains</ServiceCardTitle>
+        </ServiceCard>
+        <ServiceCard image={imga}>
+          <ServiceCardTitle>Excavation et terrasement</ServiceCardTitle>
+        </ServiceCard>
+        <OtherServiceCard>
+          <OtherServiceHeading>Et plus encore...</OtherServiceHeading>
+          <OtherServiceTitle>Drains Français</OtherServiceTitle>
+          <OtherServiceTitle>Dalle de béton</OtherServiceTitle>
+          <OtherServiceTitle>Pavé</OtherServiceTitle>
+          <OtherServiceTitle>Démolition</OtherServiceTitle>
+        </OtherServiceCard>
+      </ServicesSection>
+    </>
+  );
 }
 
 const Title = styled.h1`
-    color: #fff;
-    font-size: 2rem;
-    font-weight: 600;
-    text-align: center;
-    padding: 1rem;
-    background-color: #121212;
+  color: #fff;
+  font-size: 2rem;
+  font-weight: 600;
+  text-align: center;
+  padding: 1rem;
+  background-color: #121212;
 `;
 
 const ServicesSection = styled.section`
@@ -54,33 +53,31 @@ const ServicesSection = styled.section`
   padding: 2rem;
   background-color: #141414;
   display: flex;
-  flex-direction: row;
   flex-wrap: wrap;
-  align-items: center;
   justify-content: center;
   gap: 2rem;
-
 `;
 
 const ServiceCard = styled.div`
   width: 350px;
   height: 250px;
   padding: 1rem;
-  background-image: linear-gradient( to bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7)), url(${(props) => props.image});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+  background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.8),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url(${(props) => props.image}) center/cover no-repeat;
   border-radius: 0.5rem;
-  padding: 1rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
+  transition: transform 0.1s ease-in-out;
 
   &:hover {
-    cursor: pointer;
     transform: scale(1.05);
-    transition: all 0.1s ease-in-out;
     background-position: top;
   }
 
@@ -111,9 +108,7 @@ const OtherServiceCard = styled.div`
   padding: 1rem;
   background-color: #101010;
   border-radius: 0.5rem;
-  padding: 1rem;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
+  margin: 2rem 0;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -126,7 +121,15 @@ const OtherServiceCard = styled.div`
   }
 `;
 
-const OtherServiceTitle = styled.h1`
+const OtherServiceHeading = styled.h2`
+  color: #fff;
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+  text-align: center;
+`;
+
+const OtherServiceTitle = styled.h3`
   color: #fff;
   font-size: 1rem;
   font-weight: 600;
@@ -135,27 +138,26 @@ const OtherServiceTitle = styled.h1`
   padding: 0.5rem;
   background-color: #1f1f1f;
   width: 100%;
-  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
+  transition: transform 0.1s ease-in-out;
 
   &:hover {
-    cursor: pointer;
     transform: scale(1.05);
-    transition: all 0.1s ease-in-out;
   }
 
   @media (max-width: 768px) {
-    font-size: 1.5rem;
-  }
-
-  @media (max-width: 425px) {
     font-size: 1.2rem;
   }
 
-  @media (max-width: 375px) {
+  @media (max-width: 425px) {
     font-size: 1rem;
+  }
+
+  @media (max-width: 375px) {
+    font-size: 0.9rem;
   }
 
   @media (max-width: 320px) {
@@ -163,7 +165,7 @@ const OtherServiceTitle = styled.h1`
   }
 `;
 
-const ServiceCardTitle = styled.h1`
+const ServiceCardTitle = styled.h2`
   color: #fff;
   font-size: 1.6rem;
   font-weight: 600;
@@ -186,4 +188,3 @@ const ServiceCardTitle = styled.h1`
     font-size: 0.8rem;
   }
 `;
-
